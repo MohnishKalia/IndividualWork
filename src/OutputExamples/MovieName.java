87f4d56961c2e42c15a8fdb572b2552b417820c2
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-09-18 08:23:19 -0500

    favoriteMovie

---
+++ src/OutputExamples/MovieName.java
@@ -0,0 +1,10 @@
+package OutputExamples;
+
+public class MovieName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Transformers 7: Bay of Michael Bay");
+	}
+
+}
