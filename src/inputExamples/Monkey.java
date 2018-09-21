8f587221251c4a106678e8f9e518a8845ca57110
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-09-21 09:42:09 -0500

    monkeyWeight2

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -4,8 +4,8 @@ public class Monkey {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double weight = -999;
 		Scanner input = new Scanner(System.in);
+		double weight = -999;
 		System.out.println("How much does an adult monkey weigh?");
 		weight = input.nextInt();
 		System.out.println("The weight is " + weight);
