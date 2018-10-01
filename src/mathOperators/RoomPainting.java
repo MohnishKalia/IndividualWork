1c0aa2f0f85028dd99c04571c268709f6a816e61
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-10-01 10:33:48 -0500

    roomPainting2

--- src/mathOperators/RoomPainting.java
+++ src/mathOperators/RoomPainting.java
@@ -7,11 +7,18 @@ public class RoomPainting {
 		Scanner input = new Scanner(System.in);
 		double width;
 		double length;
-		System.out.println("What is the width of the room?");
+		double area;
+		int gallons;
+		System.out.println("What is the width of the room (ft)?");
 		width = input.nextDouble();
-		System.out.println("What is the length of the room?");
+		System.out.println("What is the length of the room (ft)?");
 		length = input.nextDouble();
-		
+		area = 16.0*width + 16.0*length;
+		gallons = (int)Math.ceil(area/150);
+		System.out.println("Dimensions of the room: " + width + " by " + length + " by 8");
+		System.out.println("Area to paint: " + area + " square feet");
+		System.out.println("Gallons needed to paint walls: " + gallons);
+		input.close();
 	}
 
 }
