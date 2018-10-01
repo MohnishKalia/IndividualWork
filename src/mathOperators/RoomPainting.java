582a180892fd93a7c0730ba251ab3213c99f8ecc
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-10-01 09:33:13 -0500

    roomPainting

---
+++ src/mathOperators/RoomPainting.java
@@ -0,0 +1,17 @@
+package mathOperators;
+import java.util.Scanner;
+public class RoomPainting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double width;
+		double length;
+		System.out.println("What is the width of the room?");
+		width = input.nextDouble();
+		System.out.println("What is the length of the room?");
+		length = input.nextDouble();
+		
+	}
+
+}
