df9c1ae000f48120c394ce34f1b19de5519e84ac
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-10-25 08:32:09 -0500

    comparisionClasses2

--- src/decisionStructures/Shoe.java
+++ src/decisionStructures/Shoe.java
@@ -6,13 +6,14 @@ public class Shoe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.println("What is your height in feet?");
+		System.out.println("What is your height in inches?");
 		double height = input.nextDouble();
 		System.out.println("What is your shoesize (US)");
 		double size = input.nextDouble();
 		double sum = height + size;
 		System.out.println("Is the sum of your height and shoesize greater than or equal to 26?");
 		System.out.println(sum >= 26);
+		System.out.println(height < 62 && size > 9);
 		input.close();
 	}
 
