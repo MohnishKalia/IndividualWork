ec40006575f9e22c456cb0119e98b3f06a7c26e2
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-11-05 08:52:16 -0600

    loops2

---
+++ src/loops/MultiplesOf2.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class MultiplesOf2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (count <= 100000) {
+			System.out.println(count);
+			count += 2;
+		}
+	}
+
+}
