68433c0771c2d1b457ebbec0974a17eca36f4d79
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-11-05 08:33:54 -0600

    loops1

---
+++ src/loops/Integers.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class Integers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (count <= 100) {
+			System.out.println(count);
+			count++;
+		}
+	}
+
+}
