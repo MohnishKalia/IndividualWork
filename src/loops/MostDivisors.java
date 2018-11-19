eaa3ab6abba1c2945ba1bc186f76aff1e33165ea
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2018-11-19 11:29:45 -0600

    nestedLoops2

--- src/loops/MostDivisors.java
+++ src/loops/MostDivisors.java
@@ -1,11 +1,14 @@
 package loops;
 
+import java.util.Scanner;
 public class MostDivisors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 		int count = 0;
-		int num = 1000;
+		System.out.println("What is your positive integer?");
+		int num = input.nextInt();
 		int largest = 0;
 		int largestNum = 0;
 		for (int arbitrary; num > 0; num--) {
@@ -21,7 +24,7 @@ public class MostDivisors {
 			count = 0;
 		}
 		System.out.println("There are " + largest + " divisors for this integer " + largestNum);
-
+		input.close();
 	}
 
 }
