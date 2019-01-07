07cec754699d277e3fab521e5f5735575de1fc26
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-01-07 08:51:47 -0600

    practiceList

---
+++ src/arrayList/NameActivity.java
@@ -0,0 +1,19 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class NameActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Bob");
+		names.add("Jane");
+		names.add(1, "Billy");
+		System.out.println("What is your name?");
+		names.add(input.nextLine());
+		input.close();
+	}
+
+}
